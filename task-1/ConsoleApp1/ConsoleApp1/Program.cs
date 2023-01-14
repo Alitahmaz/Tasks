27e3744152c586e792fe8a6aa94796be0661005b
Alitahmaz <alirt@code.edu.az>
2023-01-14 20:14:35 +0300

    first commit

---
+++ task-1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    class task1
+    {
+        static void Main(string[] args)
+        {
+            //task1;
+            //Console.Write("n = ");
+            //var n = Convert.ToInt32(Console.ReadLine());
+            //var sum = 0;
+            //while (n > 0)
+            //{
+            //    sum += n % 10;
+            //    n /= 10;
+            //}
+            //Console.WriteLine(sum);
+
+            //task2
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 99999 && a < 1000000)
+            //{
+            //    int b = a / 1000;
+            //    int c = b % 10;
+            //    b = b / 10;
+            //    int e = b % 10;
+            //    b = b / 10;
+            //    int r = b % 10;
+            //    int sum = (c + e + r);
+            //    Console.WriteLine(sum);
+
+            //}
+
+            // }
+            //task3
+            //    Console.Write(a);
+            //}
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 99999999 && a < 1000000000)
+            //{
+            //    int b = a / 1000;
+            //    int c = b % 1000;
+            //    Console.WriteLine(c);
+            //}
+            //task4
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 9999 && a < 100000)
+            //{
+            //    int b = a % 10;
+
+            //    int c = a / 10000;
+            //    int sum = b + c;
+            //    sum =(int) Math.Pow(sum, 2);
+            //    Console.WriteLine(sum);
+            //}
+            //task6
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 9999999 && a < 100000000)
+            //{
+            //    int b = a % 10000000;
+            //    b = b / 10;
+            //    Console.WriteLine(b);
+
+            // }
+            //task7
+            //int ters = 0;
+            //Console.WriteLine("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 999 && a < 10000)
+            //{
+            //    while (a > 0)
+            //    {
+            //        int son = a % 10;
+            //        ters = ters * 10 + son;
+            //        a /= 10;
+
+            //    }
+            //    ters = ters * 10 + 8;
+            //    Console.WriteLine(ters);
+
+
+            //  }
+            //task 8 
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 999 && a < 10000)
+            //{
+            //    int c = a % 1000;
+
+            //    int b = c / 100;
+            //    Console.WriteLine(b);
+            //    int d = a % 10;
+            //    Console.WriteLine(d);
+            //    int sum = b + d;
+            //    Console.WriteLine(sum);
+
+            // }
+            //task9
+            //Console.Write("a: ");
+
+            //int a = int.Parse(Console.ReadLine());
+            //int son = 0, tersA = 0;
+            //while (a > 0)
+            //{
+            //    son = a % 10;
+            //    tersA = tersA * 10 + son;
+            //    a /= 10;
+
+            //}
+            //int part1 =0, part2 = 0;
+            //int i = 1;
+            //while (tersA > 0)
+            //{
+            //    son = tersA % 10;
+            //    tersA /= 10;
+            //    if (i % 2 == 0)
+            //    {
+            //        part2 = part2 * 10 + son;
+            //    }
+            //    if (i % 2 != 0)
+            //    {
+            //        part1 = part1 * 10 + son;
+            //    }
+            //    i++;
+
+
+            //}
+            //Console.WriteLine(part1);
+            //Console.WriteLine(part2);
+            //task10
+            //Console.Write("a: ");
+
+            //int a = int.Parse(Console.ReadLine());
+            //int son = 0, tersA = 0;
+            //while (a > 0)
+            //{
+            //    son = a % 10;
+            //    tersA = tersA * 10 + son;
+            //    a /= 10;
+
+            //}
+            //int part1 =0, part2 = 0;
+            //int i = 1;
+            //while (tersA > 0)
+            //{
+            //    son = tersA % 10;
+            //    tersA /= 10;
+            //    if (i % 2 == 0)
+            //    {
+            //        part2 = part2 * 10 + son;
+            //    }
+            //    if (i % 2 != 0)
+            //    {
+            //        part1 = part1 * 10 + son;
+            //    }
+            //    i++;
+            //int sum = part1 + part2;
+            //Console.WriteLine(sum);
+            //task11
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 9999999 && a < 100000000)
+            //{
+            //    int b = a / 10000;
+            //    int c = a % 10000;
+            //    int sum = b + c;
+            //    int axiri = sum * 100 + 99;
+            //    int faiz = axiri / 100 * 18;
+            //    Console.WriteLine(faiz);
+
+            //}
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b:");
+            
+           
+            //int b = int.Parse(Console.ReadLine());
+            //if (a > 9999 && a < 100000)
+          
+            //{
+
+            //    int q = a % 10;
+            //    a /= 10;
+            //    int c = a % 10;
+            //    a /= 10;
+            //    int w = a % 10;
+            //    a /= 10;
+            //    int e =a % 10;
+            //    a /= 10;
+            //    int r = a % 10;
+            //    int sum = q+c+w+e+r;
+
+            //    Console.WriteLine(sum);
+            //}
+            //if (b > 9999 && b < 100000)
+          
+            //{
+
+            //    int q = b % 10;
+            //    b /= 10;
+            //    int c = b % 10;
+            //    b /= 10;
+            //    int w = b % 10;
+            //    b /= 10;
+            //    int e = b % 10;
+            //    b /= 10;
+            //    int r = b % 10;
+            //    int vurma = q * c * w * e * r;
+            //    Console.WriteLine(vurma);
+
+            //}
+            
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
